4e619705e977068550e387683916a11d11560f9e
WollasRomeiro <wollasromeiro@icloud.com>
2022-04-10 21:28:32 -0300

    first commit

---
+++ teste1.js
@@ -0,0 +1,5 @@
+let nome = "João"
+let idade = 17
+    console.log("Olá, " + nome)
+if (idade > 18) 
+    console.log("Você passou no nosso teste e já pode dirigir!")
---
+++ teste3.js
@@ -0,0 +1,5 @@
+let idade = 17
+if (idade >= 18)
+    console.log('Você é obrigado a votar')
+if (idade < 18 && idade >= 16) 
+    console.log('Seu voto é opcional')
