3379f68500682fa8995a9dfdacba3fae1a61b3b7
WollasRomeiro <wollasromeiro@icloud.com>
2022-04-11 23:33:19 -0300

    Firts commit

--- teste3.js
+++ teste3.js
@@ -1,5 +1,6 @@
-let idade = 17
-if (idade >= 18)
-    console.log('Você é obrigado a votar')
-if (idade < 18 && idade >= 16) 
-    console.log('Seu voto é opcional')
+let idade = 19
+if (idade >= 18){
+    console.log('Você é obrigado a votar')}
+if (idade == 16){
+    console.log('Seu voto é opcional')}  
+    
