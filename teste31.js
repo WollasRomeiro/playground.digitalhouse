e22ca2fbf19ea981526911d979ae7d667dda2026
WollasRomeiro <wollasromeiro@icloud.com>
2022-04-17 14:53:47 -0300

    update

---
+++ teste31.js
@@ -0,0 +1,14 @@
+function validaCPF(cpf){
+
+    var cpf = "576.524.020-85"
+    
+    if (validaCPF(cpf)){
+
+        console.log("CPF válido")
+    } else {
+
+        console.log("CPF inválido")
+
+    }
+
+}
---
+++ teste35.js
@@ -0,0 +1,18 @@
+/*Escreva uma função maxmin(a, b, c, d, e) que receba cinco números como parâmetros.
+Sua função deve retornar um array onde o primeiro elemento é o menor dos parâmetros da função maxmin 
+e o segundo elemento é o maior dos parâmetros da função maxmin. */
+
+
+function maxmin(a, b, c, d, e){
+
+   array = [];
+
+   for(var i = 0; i < 5; i++){
+
+       return array.push(Math.min(maxmin).push(Math.max(maxmin)));
+    
+    }
+
+    return array
+
+}
