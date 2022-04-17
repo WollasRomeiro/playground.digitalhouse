fe5b18a1b56d59a6a29c7e31ee912dc1de7d336d
WollasRomeiro <wollasromeiro@icloud.com>
2022-04-17 15:41:07 -0300

    update

---
+++ teste40.js
@@ -0,0 +1,20 @@
+/*Em uma  academia estão cadastrando nome, idade e altura de vários usuários.
+Um determinado treino exige os seguintes requisitos: Ter 18 anos ou mais e ter uma altura igual ou maior a 1,70.
+Crie uma função chamada maiorAlto. Essa função irá receber um array como parâmetro. 
+Este array terá o nome na primeira posição, a idade na segunda posição e a altura em centímetros na terceira posição.
+Ela também deve retornar verdadeiro (true) caso o aluno atenda os requisitos, ou falso (false) caso contrário.*/
+
+
+
+function maiorAlto(array) {
+
+    if(array[1] >= 18 && array[2] >=170){
+        
+        return true
+    
+    } else {
+        
+        return false
+    }
+
+}
---
+++ teste41.js
@@ -0,0 +1,41 @@
+/*Uma academia precisa separar grupos de pessoas tendo como base as suas alturas. 
+Temos um array alunos que contem a altura de cada aluno da academia.
+Temos outros 3 arrays: grupoA, grupoB e grupoC. Eles devem ser preenchidos da seguinte forma: 
+
+grupoA - Alunos com altura entre 150 a 159
+grupoB - Alunos com altura entre 160 a 169
+grupoC - Alunos com altura de 170 ou mais
+
+Seu trabalho é pegar cada valor do array alunos e colocar o valor correspondente em seu respectivo grupo. 
+Para esse exercício, você precisará usar loops, condicionais e funções de arrays. */
+
+
+var alunos = [170, 159, 151, 187, 156, 191, 165, 154, 167, 169, 171, 170, 160]
+
+var grupoA = [159, 151, 156, 154 ];
+
+var grupoB = [165, 167, 169, 160 ];
+
+var grupoC = [170, 187, 191, 171, 170 ];
+
+function zumbaClass(alunos) {
+
+    for (var i = 0; i < alunos.length; i++) {
+  
+        if (grupoA[i] > 150 && alunos[i] <= 159) {
+  
+            grupoA.push(alunos[i])
+  
+        } else if (grupoB[i] > 159 && alunos[i] < 170) {
+  
+            grupoB.push(alunos[i])
+  
+        } else if (grupoC[i] >= 170) {
+  
+            grupoC.push(alunos[i])
+  
+        }
+  
+    }
+  
+  }
